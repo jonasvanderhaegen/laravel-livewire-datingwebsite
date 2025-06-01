1d989134fa16c92d2fc1db2a0e3dd101203dbdbd
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2025-06-01 17:10:02 +0200

    rector and lint module

--- Modules/Core/Http/Controllers/CoreController.php
+++ Modules/Core/Http/Controllers/CoreController.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Core\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CoreController extends Controller
+final class CoreController extends Controller
 {
     /**
      * Display a listing of the resource.
