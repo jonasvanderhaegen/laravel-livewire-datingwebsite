b23f9819907eed6a503944dcd40eedd2032830c6
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2025-06-01 16:02:22 +0200

    Merge tests in 1 yml file, call composer script

--- rector.php
+++ rector.php
@@ -2,39 +2,46 @@
 
 declare(strict_types=1);
 
-use Rector\Caching\ValueObject\Storage\FileCacheStorage;
-use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
-use Rector\ValueObject\PhpVersion;
+use Rector\Exception\Configuration\InvalidConfigurationException;
 use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    // Paths to analyze
-    $rectorConfig->paths([
-        __DIR__.'/app',
-        __DIR__.'/config',
-        __DIR__.'/database',
-        __DIR__.'/resources',
-        __DIR__.'/routes',
-        __DIR__.'/tests',
-    ]);
+try {
+    return RectorConfig::configure()
 
-    // Skip specific rules
-    $rectorConfig->skip([
-        CompactToVariablesRector::class,
-    ]);
+        ->withPaths([
+            __DIR__.'/app',
+            __DIR__.'/bootstrap/app.php',
+            __DIR__.'/config',
+            __DIR__.'/public',
+            __DIR__.'/resources',
+            __DIR__.'/routes',
+            __DIR__.'/tests',
+        ])
 
-    // Enable caching for Rector
-    $rectorConfig->cacheDirectory(__DIR__.'/storage/rector');
-    $rectorConfig->cacheClass(FileCacheStorage::class);
+        // Enable caching for Rector
+        ->withCache(__DIR__.'/storage/rector')
 
-    // Apply sets for Laravel and general code quality
-    $rectorConfig->sets([
-        LaravelLevelSetList::UP_TO_LARAVEL_120,
-        SetList::CODE_QUALITY,
-    ]);
+        // Apply sets for Laravel and general code quality
+        ->withSets([
+            LaravelLevelSetList::UP_TO_LARAVEL_120,
+            LaravelSetList::LARAVEL_CODE_QUALITY,
+            LaravelSetList::LARAVEL_COLLECTION,
+            // //LaravelSetList::LARAVEL_STATIC_TO_INJECTION,
+            // LaravelSetList::LARAVEL_IF_HELPERS,
+            // LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+            // LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+            // LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+            // LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+            // LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL
+        ])
+
+        ->withRules([])
 
     // Define PHP version for Rector
-    $rectorConfig->phpVersion(PhpVersion::PHP_84);
-};
+        ->withPhpSets(php84: true);
+
+} catch (InvalidConfigurationException  $e) {
+
+}
