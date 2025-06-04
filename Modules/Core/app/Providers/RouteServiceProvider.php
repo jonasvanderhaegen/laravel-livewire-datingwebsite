2d0c1dd331bdaf171e711457ac88f24699e290ae
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2025-06-04 16:16:02 +0200

    Add spatie responsecache to composer.json and add middleware to routeserviceprovider of module Core

--- Modules/Core/app/Providers/RouteServiceProvider.php
+++ Modules/Core/app/Providers/RouteServiceProvider.php
@@ -10,6 +10,8 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\RateLimiter;
 use Modules\Core\Http\Middleware\DetectDevice;
+use Spatie\ResponseCache\Middlewares\CacheResponse;
+use Spatie\ResponseCache\Middlewares\DoNotCacheResponse;
 
 final class RouteServiceProvider extends ServiceProvider
 {
@@ -25,24 +27,29 @@ final class RouteServiceProvider extends ServiceProvider
         parent::boot();
 
         $router = $this->app->make(Router::class);
+        $router->aliasMiddleware('doNotCacheResponse', DoNotCacheResponse::class);
+
         $router->pushMiddlewareToGroup('web', DetectDevice::class);
+        $router->pushMiddlewareToGroup('web', CacheResponse::class);
 
         // 10 requests / minute for guest auth pages (login/register/reset)
         RateLimiter::for('guest-auth',
-            fn (Request $r) => Limit::perMinute(10)->by($r->ip()));
+            fn(Request $r) => Limit::perMinute(10)->by($r->ip()));
 
         // 30 requests / minute across user settings & messages
         RateLimiter::for('user-actions',
-            fn (Request $r) => Limit::perMinute(30)->by(optional($r->user())->id ?: $r->ip()));
+            fn(Request $r) => Limit::perMinute(30)->by(optional($r->user())->id ?: $r->ip()));
 
         // 5 posts / minute for contact form
         RateLimiter::for('contact-form',
-            fn (Request $r) => Limit::perMinute(5)->by($r->ip()));
+            fn(Request $r) => Limit::perMinute(5)->by($r->ip()));
 
     }
 
     /**
      * Define the routes for the application.
      */
-    public function map(): void {}
+    public function map(): void
+    {
+    }
 }
