6263ec6e1c0f8dfa14509eda3b8e34142bd643da
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2025-06-06 11:05:58 +0200

    Add middle circle for longer pages

---
+++ Modules/CustomTheme/resources/views/components/middle.blade.php
@@ -0,0 +1,9 @@
+{{-- Add this right after top.blade.php on much longer pages --}}
+<div
+    class="pointer-events-none absolute top-[200vh] -right-[20%] h-[100vw] max-h-[2000px] min-h-[800px] w-[100vw] max-w-[2000px] min-w-[800px] overflow-hidden rounded-full"
+>
+    <div class="bg-noise absolute top-0 left-0 -z-1 h-full w-full"></div>
+    <div
+        class="absolute top-0 left-0 -z-2 h-full w-full -rotate-[30] transform bg-gradient-to-tl from-[#3781FE] to-[#1C0AC4]/0 to-80%"
+    ></div>
+</div>
