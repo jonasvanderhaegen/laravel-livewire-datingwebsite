9c1b420aeca4b9bd0e7271c1d72c5ee92ecf7017
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2025-06-09 01:33:01 +0200

    Adjust blade views

--- Modules/CustomTheme/resources/views/components/page-partials/page-with-image-banner/header-title.blade.php
+++ Modules/CustomTheme/resources/views/components/page-partials/page-with-image-banner/header-title.blade.php
@@ -1,14 +1,16 @@
-<div
+<x-flowbite::boxed-width
     @class([
-        'mx-auto w-full max-w-5xl xl:max-w-7xl 2xl:max-w-[90rem]',
-        'absolute top-20 left-1/2 -translate-x-1/2 px-4 text-white xl:top-1/2 xl:-translate-y-1/2 xl:px-0' => $image,
+        'w-full',
+        'absolute top-20 left-1/2 -translate-x-1/2 px-4 text-white xl:top-1/2 xl:-translate-y-1/2' => $image,
         'relative text-slate-900 dark:text-white' => ! $image,
     ])
 >
-    <h1
-        class="mb-4 max-w-4xl text-2xl leading-none font-extrabold sm:text-3xl lg:text-4xl"
-    >
-        {{ $title }}
-    </h1>
-    <p class="text-lg font-bold">{!! $description !!}</p>
-</div>
+    <x-slot name="body">
+        <h1
+            class="mb-4 max-w-4xl text-2xl leading-none font-extrabold sm:text-3xl lg:text-4xl"
+        >
+            {{ $title }}
+        </h1>
+        <p class="text-lg font-bold">{!! $description !!}</p>
+    </x-slot>
+</x-flowbite::boxed-width>
